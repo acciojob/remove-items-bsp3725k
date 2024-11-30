acf68eb659d2df94e9eb3d7e173524d3bf038f43
bsp3725k <139534037+bsp3725k@users.noreply.github.com>
2024-11-30 10:15:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-//your JS code here. If required.
+let button = document.getElementById("tap&remove");
+let dropdown = document.getElementById("colorSelect");
+
+button.addEventListener("click", () => {
+          // Get the selected index
+            let selectedIndex = dropdown.selectedIndex;
+
+            // Remove the selected option
+            if (selectedIndex >= 0) {
+                dropdown.remove(selectedIndex);
+            }
+});
